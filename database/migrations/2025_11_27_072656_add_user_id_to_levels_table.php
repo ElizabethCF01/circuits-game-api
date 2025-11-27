d19199c0ca51635c4142d38fcf41240139a549ae
ElizabethCF01 <elizabethcalzadillafernandez@gmail.com>
2025-11-27 14:41:01 +0100

    REMOVE :bug: Adjust user_id foreign key constraint in levels migration to prevent cascading deletes

--- database/migrations/2025_11_27_072656_add_user_id_to_levels_table.php
+++ database/migrations/2025_11_27_072656_add_user_id_to_levels_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('levels', function (Blueprint $table) {
-            $table->foreignId('user_id')->after('id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->after('id');
         });
     }
 
