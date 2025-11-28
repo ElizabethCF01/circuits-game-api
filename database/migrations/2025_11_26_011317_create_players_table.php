719735d765580ea8e7084ff2ad7a61ab0b1ac498
ElizabethCF01 <elizabethcalzadillafernandez@gmail.com>
2025-11-28 11:19:13 +0100

    ADD :sparkles: Update players table migration to enforce unique constraint on user_id

--- database/migrations/2025_11_26_011317_create_players_table.php
+++ database/migrations/2025_11_26_011317_create_players_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('players', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
+            $table->foreignId('user_id')->unique()->constrained()->onDelete('cascade');
             $table->string('nickname')->unique();
             $table->integer('xp')->default(0);
             $table->timestamps();
