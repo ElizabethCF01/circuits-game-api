e0b2359c5b414de56715cef3705327f4f7217cf2
ElizabethCF01 <elizabethcalzadillafernandez@gmail.com>
2026-01-25 14:10:05 +0100

    ADD :sparkles: Schedule daily backup commands for database maintenance

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('backup:clean')->dailyAt('03:10')->timezone('Europe/Madrid');
+Schedule::command('backup:run --only-db')->dailyAt('03:15')->timezone('Europe/Madrid');
+// Schedule::command('backup:run')->sundays()->at('03:20')->timezone('Europe/Madrid');
